3b2eab377b11d1dca6f88c50a7212faca98ac9a5
nnokarazu12 <nnokarazu@gmail.com>
2021-05-10 18:57:41 -0400

    Commit

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const port = 3000;
+const path = require('path');
 app.use(express.json());
 
 const fs = require('fs');
 
 const inventory = require('./inventory.json');
+//const html = require('./index.html');
 
 app.get("/", (req, res) => {
-    res.send("Welcome.");
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get("/api/inventory", (req, res) => {
@@ -36,6 +38,6 @@ app.post("/api/inventory", (req, res) => {
     })
 });
 
-app.listen(PORT, () => {
-    console.log(`Running on port ${PORT}.`);
+app.listen(port, () => {
+    console.log(`Running on port ${port}.`);
 });
